ef7e5140ccccae781c89474073a7f660904c05f8
Mike Vondrak <me@mikevondrak.com>
2025-02-20 07:24:33 -0700

    WIP - layout refactor, min size done

--- src/app/components/page-parts/site-sidebar/site-sidebar.component.ts
+++ src/app/components/page-parts/site-sidebar/site-sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-site-sidebar',
@@ -7,6 +7,20 @@ import { Component } from '@angular/core';
   templateUrl: './site-sidebar.component.html',
   styleUrl: './site-sidebar.component.scss'
 })
-export class SiteSidebarComponent {
+export class SiteSidebarComponent implements OnInit {
+  /**
+   * Layout property passed from App, specifies overlap of title element and "corner" section
+   */
+  @Input() offsetVertical: string = '';
+  /**
+   * CSS variable that maps to headerInternalVerticalOffset input
+   */
+  @HostBinding('style.--offsetVerticalVar') offsetVerticalProp = this.offsetVertical;
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.offsetVerticalProp = this.offsetVertical;
+    console.log('SIDEBAR', this.offsetVerticalProp, this.offsetVertical);
+  }
 }
